8c3d3ddddcfd52c1c19d72f2f9b1ca0ac73b828a
stoveleague <stoveleague.com>
2022-03-24 17:40:04 +0900

    update220324

--- ProjectCorona.py
+++ ProjectCorona.py
@@ -5,7 +5,7 @@ matplotlib.rcParams['font.size'] = 15
 matplotlib.rcParams['axes.unicode_minus'] = False
 
 import pandas as pd
-df = pd.read_excel('코로나바이러스감염증-19_확진환자_발생현황_220323.xlsx', skiprows=[0,1,2,3,5])
+df = pd.read_excel('코로나바이러스감염증-19_확진환자_발생현황_220324.xlsx', skiprows=[0,1,2,3,5])
 df.replace('-', 0, inplace=True)
 
 df['누적확진'] = df['계(명)'].cumsum()
@@ -18,59 +18,59 @@ df30 = df.tail(30)
 x = df['일자']; y = df['계(명)']
 plt.title('대한민국 코로나19 일일확진자')
 plt.plot(x, y, color='k')
-plt.xticks(['2020-03-01', '2021-03-01', '2022-03-01'])
+plt.xticks(rotation=45)
 plt.show()
 
 x = df30['일자']; y = df30['계(명)']
 plt.title('대한민국 코로나19 일일확진자 최근 30일')
 plt.plot(x, y, color='k')
-plt.xticks(['2022-02-23', '2022-03-09', '2022-03-23'])
+plt.xticks(rotation=45)
 plt.show()
 
 x = df['일자']; y = df['누적확진']
 plt.title('대한민국 코로나19 누적확진자')
 plt.plot(x, y, color='k')
-plt.xticks(['2020-03-01', '2021-03-01', '2022-03-01'])
+plt.xticks(rotation=45)
 plt.show()
 
 x = df['일자']; y = df['발생률']
 plt.title('대한민국 코로나19 발생률')
 plt.plot(x, y, color='k')
-plt.xticks(['2020-03-01', '2021-03-01', '2022-03-01'])
+plt.xticks(rotation=45)
 plt.show()
 
 x = df30['일자']; y = df30['발생률']
 plt.title('대한민국 코로나19 발생률 최근30일')
 plt.plot(x, y, color='k')
-plt.xticks(['2022-02-23', '2022-03-09', '2022-03-23'])
+plt.xticks(rotation=45)
 plt.show()
 
 x = df['일자']; y = df['사망(명)']
 plt.title('대한민국 코로나19 일일사망자')
 plt.plot(x, y, color='k')
-plt.xticks(['2020-03-01', '2021-03-01', '2022-03-01'])
+plt.xticks(rotation=45)
 plt.show()
 
 x = df30['일자']; y = df30['사망(명)']
 plt.title('대한민국 코로나19 일일사망자 최근 30일')
 plt.plot(x, y, color='k')
-plt.xticks(['2022-02-23', '2022-03-09', '2022-03-23'])
+plt.xticks(rotation=45)
 plt.show()
 
 x = df['일자']; y = df['누적사망']
 plt.title('대한민국 코로나19 누적사망자')
 plt.plot(x, y, color='k')
-plt.xticks(['2020-03-01', '2021-03-01', '2022-03-01'])
+plt.xticks(rotation=45)
 plt.show()
 
 x = df['일자']; y = df['치명률']
 plt.title('대한민국 코로나19 치명률')
 plt.plot(x, y, color='k')
-plt.xticks(['2020-03-01', '2021-03-01', '2022-03-01'])
+plt.xticks(rotation=45)
 plt.show()
 
 x = df30['일자']; y = df30['치명률']
 plt.title('대한민국 코로나19 치명률 최근 30일')
 plt.plot(x, y, color='k')
-plt.xticks(['2022-02-23', '2022-03-09', '2022-03-23'])
+plt.xticks(rotation=45)
 plt.show()
