e9b5a690e2aa3c9874b32a82aa87a35981d9ab8b
stoveleague <stoveleague.com>
2022-03-24 20:25:30 +0900

    그래프 변경, x좌표 눈금 변경

--- ProjectCorona.py
+++ ProjectCorona.py
@@ -6,71 +6,72 @@ matplotlib.rcParams['axes.unicode_minus'] = False
 
 import pandas as pd
 df = pd.read_excel('코로나바이러스감염증-19_확진환자_발생현황_220324.xlsx', skiprows=[0,1,2,3,5])
+df.drop(columns=['국내발생(명)', '해외유입(명)'], inplace=True)
+df.rename(columns={'계(명)':'일일확진자', '사망(명)':'일일사망자'}, inplace=True)
+df['일자'] = df['일자'].dt.strftime('%y.%m.%d')
 df.replace('-', 0, inplace=True)
-
-df['누적확진'] = df['계(명)'].cumsum()
-df['누적사망'] = df['사망(명)'].cumsum()
-df['발생률'] = df['누적확진'] / 51625561 * 100
-df['치명률'] = df['누적사망'] / df['누적확진'] * 100
-
+df['누적확진자'] = df['일일확진자'].cumsum()
+df['누적사망자'] = df['일일사망자'].cumsum()
+df['발생률'] = df['누적확진자'] / 51625561 * 100
+df['치명률'] = df['누적사망자'] / df['누적확진자'] * 100
 df30 = df.tail(30)
 
-x = df['일자']; y = df['계(명)']
+x = df['일자']; y=df['일일확진자']
 plt.title('대한민국 코로나19 일일확진자')
 plt.plot(x, y, color='k')
-plt.xticks(rotation=45)
+plt.xticks(range(0, len(x), len(x)//4))
 plt.show()
 
-x = df30['일자']; y = df30['계(명)']
+x = df30['일자']; y=df30['일일확진자']
 plt.title('대한민국 코로나19 일일확진자 최근 30일')
 plt.plot(x, y, color='k')
-plt.xticks(rotation=45)
+plt.xticks(range(0, len(x), len(x)//4))
 plt.show()
 
-x = df['일자']; y = df['누적확진']
+x = df['일자']; y=df['누적확진자']
 plt.title('대한민국 코로나19 누적확진자')
 plt.plot(x, y, color='k')
-plt.xticks(rotation=45)
+plt.xticks(range(0, len(x), len(x)//4))
 plt.show()
 
-x = df['일자']; y = df['발생률']
+x = df['일자']; y=df['발생률']
 plt.title('대한민국 코로나19 발생률')
 plt.plot(x, y, color='k')
-plt.xticks(rotation=45)
+plt.xticks(range(0, len(x), len(x)//4))
 plt.show()
 
-x = df30['일자']; y = df30['발생률']
+x = df30['일자']; y=df30['발생률']
 plt.title('대한민국 코로나19 발생률 최근30일')
 plt.plot(x, y, color='k')
-plt.xticks(rotation=45)
+plt.xticks(range(0, len(x), len(x)//4))
 plt.show()
 
-x = df['일자']; y = df['사망(명)']
+x = df['일자']; y=df['일일사망자']
 plt.title('대한민국 코로나19 일일사망자')
 plt.plot(x, y, color='k')
-plt.xticks(rotation=45)
+plt.xticks(range(0, len(x), len(x)//4))
 plt.show()
 
-x = df30['일자']; y = df30['사망(명)']
+x = df30['일자']; y=df30['일일사망자']
 plt.title('대한민국 코로나19 일일사망자 최근 30일')
 plt.plot(x, y, color='k')
-plt.xticks(rotation=45)
+plt.xticks(range(0, len(x), len(x)//4))
 plt.show()
 
-x = df['일자']; y = df['누적사망']
+x = df['일자']; y=df['누적사망자']
 plt.title('대한민국 코로나19 누적사망자')
 plt.plot(x, y, color='k')
-plt.xticks(rotation=45)
+plt.xticks(range(0, len(x), len(x)//4))
 plt.show()
 
-x = df['일자']; y = df['치명률']
+x = df['일자']; y=df['치명률']
 plt.title('대한민국 코로나19 치명률')
 plt.plot(x, y, color='k')
-plt.xticks(rotation=45)
+plt.xticks(range(0, len(x), len(x)//4))
 plt.show()
 
-x = df30['일자']; y = df30['치명률']
+x = df30['일자']; y=df30['치명률']
 plt.title('대한민국 코로나19 치명률 최근 30일')
 plt.plot(x, y, color='k')
-plt.xticks(rotation=45)
+plt.xticks(range(0, len(x), len(x)//4))
 plt.show()
